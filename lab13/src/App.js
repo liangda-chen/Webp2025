5a4e1ccdf0d1117584a44862d1476369eaf27b7c
liangda-chen <amida18706@gmail.com>
2025-05-07 15:06:04 +0800

    Add files via upload

---
+++ lab13/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import CGU_Login from './cgu_login';
+import HelloCGU from './cgu_hello';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        {CGU_Login()}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ lab13/src/cgu_hello.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const styleArgument = { 
+  fontSize: '100px', 
+  color: 'red' 
+};
+
+const HelloCGU = () => {
+  return <h1 style={styleArgument}>Hello CGU!!</h1>;
+};
+
+export default HelloCGU;
